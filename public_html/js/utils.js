6d406a2a6900f40f9178287d960518c15e8a2dff
Anders Evenrud <andersevenrud@gmail.com>
2011-09-05 03:13:29 +0200

    Cross browser compability (events)

--- public_html/js/utils.js
+++ public_html/js/utils.js
@@ -13,14 +13,32 @@ var $ = (function(undefined) {
 
   return {
     'mousePosX' : function(ev, el) {
-      return ev.clientX;
+      ev = ev || window.event;
+
+      var x = 0;
+      if ( ev.pageX !== undefined ) {
+        x = ev.pageX;
+      } else if ( ev.clientX !== undefined ) {
+        x = ev.clientX + document.body.scrollLeft + document.documentElement.scrollLeft;
+      }
+      return x; // ev.clientX
     },
 
     'mousePosY' : function(ev, el) {
-      return ev.clientY;
+      ev = ev || window.event;
+
+      var y = 0;
+      if ( ev.pageY !== undefined ) {
+        y = ev.pageY;
+      } else if ( ev.clientY !== undefined ) {
+        y = e.clientY + document.body.scrollTop + document.documentElement.scrollTop;
+      }
+      return y; // ev.clientY
     },
 
     'mouseButton' : function(ev) {
+      ev = ev || window.event;
+
       var button = 1;
       if ( ev.which ) {
         button= (ev.which < 2) ? 1 : ((ev.which == 2) ? 2 : 3);
@@ -30,34 +48,50 @@ var $ = (function(undefined) {
       return button;
     },
 
-    'addEvent'  : function(el, ev, callback) {
-      /*
+    'addEvent'  : function(el, ev, callback, arg) {
+      ev = ev || window.event;
+      arg == undefined ? false : arg;
+
       if ( el.addEventListener ) {
-        el.addEventListener(ev, callback);
-      } else if ( el.addEvent ) {
-        el.addEvent("on" + ev, callback);
+        el.addEventListener(ev, callback, arg);
+      } else if ( el.attachEvent ) {
+        el.attachEvent("on" + ev, callback);
+      } else {
+        el["on" + ev] = callback;
       }
-      */
-      el["on" + ev] = callback;
     },
 
-    'removeEvent' : function(el, ev, callback) {
-      /*
+    'removeEvent' : function(el, ev, callback,arg ) {
+      ev = ev || window.event;
+      arg == undefined ? false : arg;
+
       if ( el.removeEventListener ) {
-        el.removeEventListener(ev, callback);
-      } else if ( el.removeEvent ) {
-        el.removeEvent("on" + ev, callback);
+        el.removeEventListener(ev, callback, arg);
+      } else if ( el.detachEvent ) {
+        el.detachEvent("on" + ev, callback);
+      } else {
+        el["on" + ev] = undefined;
       }
-      */
-      el["on" + ev] = null;
     },
 
     'stopPropagation' : function(ev) {
-      ev.stopPropagation();
+      ev = ev || window.event;
+
+      if ( ev.stopPropagation ) {
+        ev.stopPropagation();
+      } else if ( ev.cancelBubble ) {
+        ev.cancelBubble = true;
+      }
     },
 
     'preventDefault' : function(ev) {
-      ev.preventDefault();
+      ev = ev || window.event;
+
+      if ( ev.preventDefault ) {
+        ev.preventDefault();
+      } else if ( ev.returnValue !== undefined ) {
+        ev.returnValue = false;
+      }
     },
 
     'disableContext' : function(el) {
